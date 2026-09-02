5bcf6fb1ae89c046d94e2a76666740af44c210c3
bjha <vickyjosh94@gmail.com>
2026-09-02 18:06:49 +0200

    added community, write in blog for users

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://independentsociety.space",
   integrations: [sitemap()],
-  output: "static"
+  output: "static",
+  adapter: vercel(),
 });
 
